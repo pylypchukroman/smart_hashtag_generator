d5a9d7a30a2173e31aa16baa685ac6b2c24b46ae
pylypchukroman <romanpylypchuk@gmail.com>
2025-08-06 20:01:50 +1000

    added render of output

--- hashtag_generator/src/api/openAiProvider.js
+++ hashtag_generator/src/api/openAiProvider.js
@@ -8,6 +8,6 @@ export const getHashtagsFromOpenAi = async (text, count) => {
     //     model: "gpt-4.1-nano",
     //     input: `Generate ${count} relevant hashtags for this post: ${text}`
     // });
-
+    //
     // return response.output_text;
 }
--- hashtag_generator/src/components/Output/Output.jsx
+++ hashtag_generator/src/components/Output/Output.jsx
@@ -1,19 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {InputContext} from "../../context/inputContext";
 import {HashtagContext} from "../../context/hashtagsContext";
 
 export const Output = () => {
     const { input, setInput } = useContext(InputContext);
     const { hashtags, setHashtags } = useContext(HashtagContext);
+    const [output, setOutput] = useState(input);
+
+    const handleButtonClick = (hash) => {
+        setOutput((prevState) => prevState ? `${prevState} ${hash}` : hash);
+    };
+
+    useEffect(() => {
+        setOutput(input);
+    }, [input]);
 
     return (
         <div>
             <p>
-                {input ? input : 'input'}
+                {output ? output : 'input'}
             </p>
-            <p>
-                {hashtags.length > 0 ? hashtags.join(' ') : 'hashtags'}
-            </p>
+            {hashtags.map(hash => (
+                <button
+                    type="button"
+                    onClick={() => handleButtonClick(hash)}
+                >
+                    {hash}
+                </button>
+                )
+            )}
         </div>
     );
 };
